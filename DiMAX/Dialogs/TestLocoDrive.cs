8b8e2fe1f1a0f32dadd8c9cc9a63b0a682c2c52c
Yuval (Trains Room) <yuval@villarakavy.com>
2012-03-05 15:36:11 +0200

    OutputManager trace default changed to Info

---
+++ DiMAX/Dialogs/TestLocoDrive.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+using LayoutManager;
+
+namespace DiMAX.Dialogs {
+    public partial class TestLocoDrive : Form {
+		DiMAXcommandStation commandStation;
+
+		public TestLocoDrive (DiMAXcommandStation commandStation) {
+			InitializeComponent();
+
+			this.commandStation = commandStation;
+        }
+
+        private void buttonOK_Click(object sender, EventArgs e) {
+            int address;
+            int speed;
+
+            if (!int.TryParse(textBoxLocoAddress.Text, out address)) {
+                MessageBox.Show("Invalid loco address");
+                return;
+            }
+
+            if(!int.TryParse(textBoxSpeed.Text, out speed)) {
+                MessageBox.Show("Invalid speed");
+                return;
+            }
+
+            EventManager.AsyncEvent(new LayoutEvent(this, "test-loco-speed").SetOption("Address", address).SetOption("Speed", speed).SetCommandStation(commandStation));
+        }
+    }
+}
