61c9c2cf640f984e0d8f7cc8f25a269522518a8d
yuval <yuval@rakavy.com>
2016-02-10 17:34:17 +0200

    Changed lock request ResourceEntries to BlockEntries

--- LayoutTools/ComponentOperationTools.cs
+++ LayoutTools/ComponentOperationTools.cs
@@ -1501,7 +1501,7 @@ namespace LayoutManager.Tools {
 				// Get lock on the block that the train is about to be placed
 				LayoutLockRequest	lockRequest = new LayoutLockRequest(extendableTrainInfo.Train.Id);
 
-				lockRequest.ResourceEntries.Add(block);
+				lockRequest.Blocks.Add(block);
 				EventManager.Event(new LayoutEvent(lockRequest, "request-layout-lock"));
 
 				TrackContactPassingStateInfo	trackContactPassingState = null;
