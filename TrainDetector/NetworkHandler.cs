f3d21f1af5756838f7adb60f3933db991aeac5ad
Yuval Rakavy (iMac Retina) <yuval_rakavy@hotmail.com>
2019-10-21 02:20:13 +0300

    Fixed bug in which delay, waiting was hanging UI Thread

--- TrainDetector/NetworkHandler.cs
+++ TrainDetector/NetworkHandler.cs
@@ -5,9 +5,13 @@ using System.IO;
 using System.Net;
 using System.Net.Sockets;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 
+using LayoutManager;
+
 #nullable enable
+#pragma warning disable IDE0051, UDE0060
 namespace TrainDetector {
     public interface IRequestReplyPacket {
         bool IsValid { get; }
@@ -17,7 +21,25 @@ namespace TrainDetector {
         void Encode(BinaryWriter w);
     }
 
-    public class NetworkHandler : IDisposable {
+    [LayoutModule("Train Detector Network handler")]
+    internal class TrainDetectorNetworkHandler : LayoutModuleBase {
+        [LayoutEvent("set-network-request-exception")]
+        private void setNetworkRequestException(LayoutEvent e) {
+            (var taskSource, var ex) = Ensure.NotNull<TaskCompletionSource<UdpReceiveResult>, Exception>(e);
+
+            taskSource.SetException(ex);
+        }
+
+        [LayoutEvent("set-network-request-reply")]
+        private void setNetworkRequestReply(LayoutEvent e) {
+            var taskSource = Ensure.NotNull<TaskCompletionSource<UdpReceiveResult>>(e.Sender);
+            var reply = Ensure.ValueNotNull<UdpReceiveResult>(e.Info);
+
+            taskSource.SetResult(reply);
+        }
+    }
+
+     public class NetworkHandler : IDisposable {
         public UdpClient UdpClient { get; private set; }
         readonly Dictionary<int, TaskCompletionSource<UdpReceiveResult>> pendingRequests = new Dictionary<int, TaskCompletionSource<UdpReceiveResult>>();
         readonly TaskCompletionSource<bool> networkHandlerDoneSource;
@@ -25,6 +47,10 @@ namespace TrainDetector {
         UInt16 nextRequestNumber = 1;
         Func<UdpReceiveResult, IRequestReplyPacket> ExtractPacket { get; set; }
         bool disposed = false;
+
+        public bool IsStarted { get; private set; } = false;
+
+        public ILayoutInterThreadEventInvoker InterThreadEventInvoker => EventManager.Instance.InterThreadEventInvoker;
 
         public NetworkHandler(Func<UdpReceiveResult, IRequestReplyPacket> extractPacket) {
             networkHandlerDoneSource = new TaskCompletionSource<bool>();
@@ -35,36 +61,40 @@ namespace TrainDetector {
 
         public event EventHandler<UdpReceiveResult>? OnReceivingPacket;
 
-        public async void Start() {
+        public void Start() {
 
-            while (true) {
-                var udpReceiveTask = UdpClient.ReceiveAsync();
+            IsStarted = true;
+            Task.Run(async () => {
+                while (true) {
+                    var udpReceiveTask = UdpClient.ReceiveAsync();
 
-                if (await Task.WhenAny(udpReceiveTask, networkHandlerDoneTask) == networkHandlerDoneTask)
-                    break;
+                    if (await Task.WhenAny(udpReceiveTask, networkHandlerDoneTask) == networkHandlerDoneTask)
+                        break;
 
-                var udpReceiveResult = await udpReceiveTask;
-                var packet = ExtractPacket(udpReceiveResult);
+                    var udpReceiveResult = await udpReceiveTask;
+                    var packet = ExtractPacket(udpReceiveResult);
 
-                if (packet.IsValid) {
-                    if (packet.IsReply) {
-                        if (pendingRequests.TryGetValue(packet.RequestNumber, out TaskCompletionSource<UdpReceiveResult>? pending)) {
-                            pending.SetResult(udpReceiveResult);
+                    if (packet.IsValid) {
+                        if (packet.IsReply) {
+                            if (pendingRequests.TryGetValue(packet.RequestNumber, out TaskCompletionSource<UdpReceiveResult>? pending)) {
+                                pending.SetResult(udpReceiveResult);
+                            }
+                            else
+                                Trace.WriteLine($"TrainDetector: got unexpected reply for request# {packet.RequestNumber}");
+
                         }
                         else
-                            Trace.WriteLine($"TrainDetector: got unexpected reply for request# {packet.RequestNumber}");
-
+                            OnReceivingPacket?.Invoke(this, udpReceiveResult);
                     }
                     else
-                        OnReceivingPacket?.Invoke(this, udpReceiveResult);
+                        Trace.WriteLine("TrainDetector: Received invalid UDP packet");
                 }
-                else
-                    Trace.WriteLine("TrainDetector: Received invalid UDP packet");
-            }
 
-            UdpClient.Close();
-            OnReceivingPacket = null;
-            Trace.WriteLine("Network handler terminated");
+                UdpClient.Close();
+                OnReceivingPacket = null;
+                IsStarted = false;
+                Trace.WriteLine("Network handler terminated");
+            });
         }
 
         public void Dispose() {
@@ -81,29 +111,39 @@ namespace TrainDetector {
             }
         }
 
-        public async Task<UdpReceiveResult> Request(Action<UInt16> sendRequestPacket, int retries = 3, int timeoutMs = 300) {
-            var doneSource = new TaskCompletionSource<UdpReceiveResult>();
-            var doneTask = doneSource.Task;
+        public Task<UdpReceiveResult> Request(Action<UInt16> sendRequestPacket, int retries = 3, int timeoutMs = 300) {
+            Debug.Assert(IsStarted);
+
+            var resultSource = new TaskCompletionSource<UdpReceiveResult>();
             var pendingRequestNumber = (UInt16)(this.nextRequestNumber++);
 
-            lock (this.pendingRequests) {
-                pendingRequests.Add(pendingRequestNumber, doneSource);
-            }
+            Task.Run(async () => {
+                try {
+                    var gotReplySource = new TaskCompletionSource<UdpReceiveResult>();
+                    var gotReplyTask = gotReplySource.Task;
 
-            for (var retry = 0; retry < retries && !doneTask.IsCompleted; retry++) {
-                sendRequestPacket(pendingRequestNumber);
-                await Task.WhenAny(doneTask, Task.Delay(timeoutMs));
-            }
+                    lock (this.pendingRequests) {
+                        pendingRequests.Add(pendingRequestNumber, gotReplySource);
+                    }
 
-            lock (this.pendingRequests) {
-                pendingRequests.Remove(pendingRequestNumber);
-            }
+                    for (var retry = 0; retry < retries && !gotReplyTask.IsCompleted; retry++) {
+                        sendRequestPacket(pendingRequestNumber);
+                        await Task.WhenAny(gotReplyTask, Task.Delay(timeoutMs)).ConfigureAwait(false);
+                    }
 
-            if (doneTask.IsCompleted)
-                return doneTask.Result;
-            else
-                throw new TimeoutException($"Request# {pendingRequestNumber} timedout");
+                    if (!gotReplyTask.IsCompleted)
+                        InterThreadEventInvoker.QueueEvent(new LayoutEvent("set-network-request-exception", resultSource, new TimeoutException($"No reply for request# {pendingRequestNumber}")));
+                    else
+                        InterThreadEventInvoker.QueueEvent(new LayoutEvent("set-network-request-reply", resultSource, gotReplyTask.Result));
+                }
+                finally {
+                    lock (this.pendingRequests) {
+                        pendingRequests.Remove(pendingRequestNumber);
+                    }
+                }
+            });
+
+            return resultSource.Task;
         }
-
     }
 }
